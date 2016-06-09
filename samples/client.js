d20a68db1745d9b0980eebeca7d6e89b4e717b4a
Lawrence Ripsher <lawrips@microsoft.com>
2016-06-08 22:24:29 -0700

    creation of a simple node client

---
+++ samples/client.js
@@ -0,0 +1,31 @@
+'use stict';
+
+const http = require('request'),
+    WebSocket = require('ws');
+
+http('http://localhost:10001/auth', {
+    method: 'get',
+    headers: {
+        authorization: 'basic a-random-password'
+    }}, (err, response, body) => {
+        if (err) {
+            console.dir(response);
+            process.exit(0);
+        }
+        console.log(`auth success, token: [${body}]`);
+        var ws = new WebSocket(`ws://localhost:10001/?token=${body}`);
+
+        ws.on('open', () => {
+            ws.send(JSON.stringify(
+                {
+                    reporter: 'basic',
+                    test: 'simple'
+                })
+            );
+        });
+
+        ws.on('message', (data, flags) => {
+            console.log(data);
+        });
+});
+        
